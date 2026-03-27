74afe2fee130eab69c07647e3c1130d749ac6a77
Florencia Riccomi <85568530+Friccomi@users.noreply.github.com>
2026-03-27 16:45:44 -0300

    Update GAS_ENDPOINT URL in main.py

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ app = Flask(__name__)
 
 
 
-GAS_ENDPOINT = "https://script.google.com/macros/s/AKfycbxZZhqHOFOdpjhiTDR_W9gm_mW7XHEKcIQoIHtKrncmW77fry_EIMqtyQ8_Uiv0gSBG/exec"   # tu URL real
+GAS_ENDPOINT = "https://script.google.com/macros/s/AKfycbxOxg1HV0N1NgNTJZZBiIV7f5DyGSnYE_B7KpoXFSNwT4lI8NsT6gdndWpfm2n1oI26/exec"   # tu URL real
 
 
 @app.route('/proxy', methods=['POST', 'OPTIONS'])
